8b518fcac5d5e681c25ace2420e0b77a70f46d6c
Charles Lien <charleslien97@gmail.com>
2021-05-02 15:55:47 -0700

    Add standard operators to Time class in time calculator

--- time_calculator.py
+++ time_calculator.py
@@ -28,6 +28,9 @@ class Time(object):
 
   def parse(time):
     if type(time) == int:
+      time %= 720
+      if time < 0:
+        time += 720
       hour = int(time // 60)
       minute = time % 60
       return Time(Time._getStandardFormat(hour, minute), time)
@@ -51,38 +54,64 @@ class Time(object):
 
     raise ValueError(f'could not convert {time} to Time')
 
-  def negate(self):
-    return Time.parse((720 - self.minute) % 720)
-
   def __repr__(self):
-    return f'Time(standard: {self.standard}, minute: {self.minute})'
+    return f'Time(standard: "{self.standard}", minute: {self.minute})'
 
   def __str__(self):
-    return repr(self)
+    return self.standard
+
+  def __int__(self):
+    return self.minute
+
+  def __add__(self, other):
+    return Time.parse(int(self) + int(other))
+
+  def __neg__(self):
+    return Time.parse(- int(self))
+
+  def __sub__(self, other):
+    return Time.parse(int(self) - int(other))
 
   def __setattr__(self, name, value):
     """ make Time immutable """
     raise AttributeError('could not set value')
 
-def _add(times):
-  return Time.parse(sum(t.minute for t in times) % 720)
+_TIME_EXPRESSION_PATTERN = re.compile(r'(?P<time>[^+-]*)(?P<operation>[+-])(?P<remaining>.*)')
+_NEGATIVE_TIME_EXPRESSION_PATTERN = re.compile(r'\s*-(?P<remaining>.*)')
 
 def evaluate(expression):
-  split_by_plus = expression.split('+')
-  # each element starts with a positive time followed by any number of negative times
-  # e.g. 1:00 - 0:30 - 0:15
-  split_by_minus = [expr.split('-') for expr in split_by_plus]
-  times_to_add = []
-  msg = []
-  for times in split_by_minus:
-    # first time is positive, all others are negative
-    head = Time.parse(times[0])
-    times_to_add.append(head)
-    msg.append(f' + {head.standard}')
-    tail = [Time.parse(t) for t in times[1:]]
-    times_to_add += [t.negate() for t in tail]
-    msg += [f' - {t.standard}' for t in tail]
-  if msg[0].startswith(' + '):
-    msg[0] = msg[0][3:]
-  print(''.join(msg))
-  return _add(times_to_add).standard
+  total = Time.parse(0)
+  to_print = []
+  prev_add = True
+  m = _NEGATIVE_TIME_EXPRESSION_PATTERN.match(expression)
+  if m:
+    expression = m.group('remaining')
+    prev_add = False
+    to_print = ['-']
+
+  m = _TIME_EXPRESSION_PATTERN.match(expression)
+  while m:
+    t = Time.parse(m.group('time'))
+    to_print.append(str(t))
+    if prev_add:
+      total += t
+    else:
+      total -= t
+
+    operation = m.group('operation')
+    prev_add = operation == '+'
+    to_print.append(operation)
+
+    expression = m.group('remaining')
+    m = _TIME_EXPRESSION_PATTERN.match(expression)
+
+  # ending time
+  t = Time.parse(expression)
+  if prev_add:
+    total += t
+  else:
+    total -= t
+  to_print.append(str(t))
+
+  print(' '.join(to_print))
+  return str(total)
