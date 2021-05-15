b5d1193d0381e423b37695fed5ed5b13b64f01c6
oozio <xxue@caltech.edu>
2021-05-15 14:05:38 +0800

    Reformat main function

--- lambda_function.py
+++ lambda_function.py
@@ -8,43 +8,58 @@ import hare.processor
 import garden.processor
 
 
+def handle_command(body):
+    # get interaction metadata
+    channel_id = body["channel_id"]
+    server_id = body["guild_id"]
+    user_id = body['member']['user']['id']
+    role_ids = body['member']['roles']
+    
+    data = body['data']
+    command = data['name']
+    
+    # add commands here; use discord_utils.get_input(data, <option name>) to access command input
+    if command == "calculate-time":
+        input = discord_utils.get_input(data, "time_string")
+        return time_calculator.evaluate(input)
+        
+    if command == "hare-puzzle":
+        guess = discord_utils.get_input(data, "guess")
+        return hare.processor.evaluateInput(channel_id, guess)
+
+    if command == "consume":
+        substance = discord_utils.get_input(data, "substance")
+        # old_size = discord_utils.get_size_role(server_id, role_ids)
+        # new_size = "Size 11"
+        # discord_utils.change_role(server_id, user_id, old_size, new_size)
+        return garden.processor.evaluateInput(channel_id, None, substance)
+
+    # vvv TEMP (REMOVE) vvv
+    if command == "channel-test":
+        arg = discord_utils.get_input(data, "arg")
+        if arg:
+          return str(arg.encode())
+        return channel_id
+    # ^^^ TEMP (REMOVE) ^^^
+
+    return f"Didn't recognize command {command}!! run away!!"
+
 def lambda_handler(event, context):
     pong = discord_utils.check_input(event)
     if pong: 
         return pong
     
     body = event["body-json"]
-    data = body["data"]
-    channel_id = body["channel_id"]
-    server_id = body["guild_id"]
+    application_id = body["application_id"]
+    interaction_token = body["token"]
     
-    command = data['name']
-
-    # add commands here; use discord_utils.get_input(data, <option name>) to access command input
+    output = "? something broke"
+    # get interaction metadata
+    try:
+        output = handle_command(body)
+        print(output)
+    except Exception as e:
+        discord_utils.update_response(application_id, interaction_token, f"Error: {e}")
+        raise e
     
-    if command == "calculate-time":
-        input = discord_utils.get_input(data, "time_string")
-        content = time_calculator.evaluate(input)
-        return discord_utils.format_response('MESSAGE_WITH_SOURCE', content)
-        
-    if command == "hare-puzzle":
-        guess = discord_utils.get_input(data, "guess")
-        output = hare.processor.evaluateInput(channel_id, guess)
-        return discord_utils.format_response('MESSAGE_WITH_SOURCE', output)
-
-    if command == "consume":
-        substance = discord_utils.get_input(data, "substance")
-        output = garden.processor.evaluateInput(channel_id, None, substance)
-        return discord_utils.format_response('MESSAGE_WITH_SOURCE', output)
-
-    # vvv TEMP (REMOVE) vvv
-    if command == "channel-test":
-        arg = discord_utils.get_input(data, "arg")
-        if arg:
-          return discord_utils.format_response('MESSAGE_WITH_SOURCE', str(arg.encode()))
-        return discord_utils.format_response('MESSAGE_WITH_SOURCE', channel_id)
-    # ^^^ TEMP (REMOVE) ^^^
-
-    return discord_utils.format_response('MESSAGE_WITH_SOURCE', "didn't recognize command!! run away!!!")
-    
-    
+    discord_utils.update_response(application_id, interaction_token, output)
